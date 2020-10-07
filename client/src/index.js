42b46581f2aaea57708e6fb2033e6dac521e8dc8
Matteo Domenici <matteodome@gmail.com>
2020-10-07 15:07:48 +0200

    - wip

--- client/src/index.js
+++ client/src/index.js
@@ -19,6 +19,7 @@ import LastPBLValue from './components/LastPBLValue'
 //import {LastPBLValue} from "./components";
 //import {actualValue} from "./data";
 import Mychart from "./components/Example/Chart/Mychart";
+import userAvatar from './Assets/user_logo.png';
 //import ExampleChart from "./components/Example/Chart/ExampleChart";
 
 //import ExampleDatatable from "./components/Example/Datatable/ExampleDatatable";
@@ -30,7 +31,7 @@ import registerServiceWorker from './registerServiceWorker';
 const userSession = {
   user: {
     name: "Matteo Domenici",
-    thumbnail: "//s.gravatar.com/avatar/f1226004485be4f88e25f5fc083fd83f?s=80"
+    thumbnail: userAvatar
   },
   items: [
     {
@@ -68,92 +69,36 @@ class AppBody extends Component {
       <Grommet theme={theme} full>
         <Box fill background="light-3">
           <AppHeader
-            appName="Kme PBL"
+            appName="Kme CMI"
             appIcon={<Menu />}
             userSession={userSession}
           />
           <Box flex overflow="auto" gap="medium" pad="medium">
-            {/*<Box
-              flex={false}
-              overflow="auto"
-              round="large"
-              background={{ color: "dark-5", opacity: "weak" }}
-              direction="row"
-              align="center"
-              pad={{ horizontal: "medium", vertical: "small" }}
-              margin={{ horizontal: "medium", top: "medium" }}
-            >
-              <Search color="brand" />
-              <TextInput plain placeholder="Search Cluster" type="search" />
-            </Box>*/}
-            <Box flex={false} align="center">
-              <Box flex={false} direction="row-responsive" overflow="auto" >
-                <Box gap="medium" flex="grow" margin="medium" width="xlarge" height="small">
+            <Box flex align="center">
+              <Box flex={false} direction="row-responsive">
+                <Box gap="small" flex="grow" margin="small" width="xlarge" height="small">
                   <LastPBLValue/>
                 </Box>
               </Box>
-              <Box flex={false} direction="row-responsive" overflow="auto">
-                <Box gap="small" flex="grow" margin="small" round pad="medium" background="white" width="xlarge" height="medium">
+              <Box flex={false} direction="row-responsive">
+                <Box gap="small" flex="grow" margin="small" round pad="medium" background="white"
+                     width="xlarge" height="medium">
                   <Mychart/>
                 </Box>
               </Box>
-
-              <Box flex={false} direction="row-responsive" overflow="auto">
-                <Box gap="small" flex="grow" margin="small" round pad="medium" background="white" width="xlarge">
-                  <Accordion flex align='center' justify='center'>
-                    <AccordionPanel label="PREZZI STORICI BASE LATTONERIA (TON)" width="large">
-                      <Box>
-                        <ExampleDatatable2/>
-                      </Box>
-                    </AccordionPanel>
-                  </Accordion>
+              <Box flex={false} direction="row-responsive">
+                <Box gap="small" flex="grow" margin="small" round pad="medium" background="white"
+                     width="xlarge">
+                  <ExampleDatatable2/>
                 </Box>
               </Box>
-                {/*<Box align='start' flex="grow" margin="medium" round pad="medium" background="white" >
-                  <Value
-                      value={(
-                          <Box direction='row' align='center' gap='xsmall'>
-                            <Text size='large' weight='bold' color='status-error'>30%</Text>
-                            <Down color='status-error' />
-                          </Box>
-                      )}
-                      label='custom value'
-                  />
-                </Box>*/}
-                {/*<Box gap="large" flex="grow" margin="medium">
-                  {utilization.map(data => (
-                    <UtilizationCard key={data.name} data={data} />
-                  ))}
-                </Box>
-                <Box flex="grow" margin="medium">
-                  <Hardware data={hardware} />
-                </Box>*/}
             </Box>
-
           </Box>
-
-          {/*<Box
-            flex={false}
-            pad={{ vertical: "xsmall", left: "medium" }}
-            responsive={false}
-            background={{ color: "brand", dark: false }}
-            direction="row"
-            align="center"
-            justify="between"
-          >
-            <Text color="light-2">Footer</Text>
-            <Button
-              size="medium"
-              label="Show Tasks"
-              icon={<Menu color="white" />}
-              reverse={true}
-            />
-          </Box>*/}
           <Footer background="dark-1" pad="small">
             <Box align="center" direction="row" gap="xsmall">
               <LineChart color="white" size="medium" />
               <Text alignSelf="center" color="white" size="small">
-                Pbl price data
+                CMI price data
               </Text>
             </Box>
             <Media />
